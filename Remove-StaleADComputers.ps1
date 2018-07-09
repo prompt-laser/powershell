7108e511ef32c4dc4bc547624e56ce0293815cab
Unknown <gillydorf@gmail.com>
2018-07-09 15:05:39 -0500

    Modified calculation for date
    Changed calculation to anything older than 6 months

--- Remove-StaleADComputers.ps1
+++ Remove-StaleADComputers.ps1
@@ -1,12 +1,12 @@
 <#
 	.SYNOPSIS
-	Automatically remove stale computer accounts from Active Directory.`
-	Requires AD module.
+	Automatically remove stale computer accounts that haven't logged in`
+	in the past 6 monthsfrom Active Directory. Requires AD module.
 #>
 
 $computers = Get-ADComputer -Filter * -Property *
 Foreach($comp in $computers){
-	if($comp.lastLogonDate -notlike "*2017*"){
+	if($comp.lastLogonDate -lt (Get-Date).AddMOnths(-6)){
 		Write-Host $comp.Name "-" $comp.lastLogonDate
 #		Remove-ADComputer -Identity $comp.Name
 	}
