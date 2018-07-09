27e38e3b5058613f2e469798bc6fb39672ed77ee
Unknown <heard_kelly@yahoo.com>
2018-07-09 14:57:44 -0500

    Updated year to 2018

--- Remove-StaleADComputers.ps1
+++ Remove-StaleADComputers.ps1
@@ -1,6 +1,6 @@
 $computers = Get-ADComputer -Filter * -Property *
 Foreach($comp in $computers){
-	if($comp.lastLogonDate -notlike "*2017*"){
+	if($comp.lastLogonDate -notlike "*2018*"){
 		Write-Host $comp.Name "-" $comp.lastLogonDate
 		Remove-ADComputer -Identity $comp.Name
 	}
