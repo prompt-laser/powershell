4759e546f5e696c218c81c24c290fe4a516b209c
Unknown <gillydorf@gmail.com>
2018-07-09 13:40:00 -0500

    Begin Fleshing Out

--- Remove-StaleADComputers.ps1
+++ Remove-StaleADComputers.ps1
@@ -1,7 +1,13 @@
+<#
+	.SYNOPSIS
+	Automatically remove stale computer accounts from Active Directory.`
+	Requires AD module.
+#>
+
 $computers = Get-ADComputer -Filter * -Property *
 Foreach($comp in $computers){
 	if($comp.lastLogonDate -notlike "*2017*"){
 		Write-Host $comp.Name "-" $comp.lastLogonDate
-		Remove-ADComputer -Identity $comp.Name
+#		Remove-ADComputer -Identity $comp.Name
 	}
 }
