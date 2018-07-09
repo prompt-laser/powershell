2b4881fd26606e2469895db95911f2f2ce931874
Unknown <gillydorf@gmail.com>
2018-07-09 15:26:22 -0500

    Added -Confirm to remove step

--- Remove-StaleADComputers.ps1
+++ Remove-StaleADComputers.ps1
@@ -8,6 +8,6 @@ $computers = Get-ADComputer -Filter * -Property *
 Foreach($comp in $computers){
 	if($comp.lastLogonDate -lt (Get-Date).AddMOnths(-6)){
 		Write-Host $comp.Name "-" $comp.lastLogonDate
-#		Remove-ADComputer -Identity $comp.Name
+		Remove-ADComputer -Identity $comp.Name -Confirm
 	}
 }
