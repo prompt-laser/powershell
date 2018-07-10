9a2c7a47100d7b89825384719ab938c1117244b3
Unknown <gillydorf@gmail.com>
2018-07-10 09:20:43 -0500

    Added -Days parameter and Comment based help

--- Remove-StaleADComputers.ps1
+++ Remove-StaleADComputers.ps1
@@ -1,12 +1,30 @@
 <#
 	.SYNOPSIS
-	Automatically remove stale computer accounts that haven't logged in`
-	in the past 6 monthsfrom Active Directory. Requires AD module.
+	Remove stale computer accounts from Active Directory. Requires AD module.
+	
+	.DESCRIPTION
+	Removes computer accounts that haven't logged in in more than the specified
+	number of days. The default is 180 days.
+	
+	.PARAMETER Days
+	Specifies minimum last logon in number of days prior to current date
+	
+	.EXAMPLE
+	Remove-StaleADComputers
+	Removes computers whose last logon was more than 180 days ago
+	
+	.EXAMPLE
+	Remove-StaleADComputers -Days 90
+	Removes computers whose last logon was more than 90 days ago
 #>
+
+param(
+	[int]$Days = 180
+)
 
 $computers = Get-ADComputer -Filter * -Property *
 Foreach($comp in $computers){
-	if($comp.lastLogonDate -lt (Get-Date).AddMOnths(-6)){
+	if($comp.lastLogonDate -lt (Get-Date).AddDays($Days * -1)){
 		Write-Host $comp.Name "-" $comp.lastLogonDate
 		Remove-ADComputer -Identity $comp.Name -Confirm
 	}
